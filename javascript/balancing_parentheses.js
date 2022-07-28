c4ca86bf0c68a946f2309f9a712fbe000390ce75
Malcolm Keyes <3.malcolmk@gmail.com>
2022-07-28 11:33:48 -0400

    Done.

--- javascript/balancing_parentheses.js
+++ javascript/balancing_parentheses.js
@@ -1,5 +1,22 @@
 function balancingParentheses(string) {
   // type your code here
+  let arr = string.split("");
+  let arrModified = true;
+
+  // checks for new pairs until it doesn't find any more
+  while (arrModified) {
+    arrModified = false;
+    for (let i = 0; i < (arr.length - 1); i++) {
+      // if found a pair of matching parentheses
+      if (arr[i] === '(' && arr[i+1] === ')') {
+        // remove them from the array
+        arr.splice(i,2);
+        arrModified = true;
+      }
+    }
+  }
+
+  return arr.length;
 }
 
 if (require.main === module) {
@@ -22,3 +39,16 @@ module.exports = balancingParentheses;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+
+// given a string of just parentheses, return the number of parenthesis
+// needed to balance them
+// that is, every left parenthesis should be followed, at some point,
+// by a right parenthesis
+
+// i.e. )()( => 2
+
+// pseudo code
+// search the code for correct, adjacent () pairs
+// if one is found, slice them out of the string
+// continue doing this until no more are found
+// return the length of the string
